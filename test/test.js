06726d415917f98ec49e90d5e66d7bab471d5f82
郭廷杰 <iamnabob@126.com>
2017-08-20 12:19:09 +0800

    ES5继承实现

--- test/test.js
+++ test/test.js
@@ -1,40 +1,41 @@
+'use strict'
 const assert = require('assert')
 
 describe('实现一个基类，可以继承，可以监听事件', function () {
-  describe('ES6', function () {
-    const Base = require('../lib/base.es6')
-    class View extends Base {
-      constructor(options) {
-        super(options)
-      }
-    }
+  // describe('ES6', function () {
+  //   const Base = require('../lib/base.es6')
+  //   class View extends Base {
+  //     constructor(options) {
+  //       super(options)
+  //     }
+  //   }
 
-    it('能够监听事件', function (done) {
-      const view = new View
-      view.on('test', function () {
-        done()
-      })
-      view.trigger('test')
-    })
+  //   it('能够监听事件', function (done) {
+  //     const view = new View
+  //     view.on('test', function () {
+  //       done()
+  //     })
+  //     view.trigger('test')
+  //   })
 
-    it('能够监听事件并传值', function (done) {
-      const view = new View
-      view.on('test', function (value) {
-        assert.equal(value, 'hello world')
-        done()
-      })
-      view.trigger('test', 'hello world')
-    })
+  //   it('能够监听事件并传值', function (done) {
+  //     const view = new View
+  //     view.on('test', function (value) {
+  //       assert.equal(value, 'hello world')
+  //       done()
+  //     })
+  //     view.trigger('test', 'hello world')
+  //   })
 
-    it('监听函数的this指向自己', function (done) {
-      const view = new View
-      view.on('test', function () {
-        assert.equal(this, view)
-        done()
-      })
-      view.trigger('test')
-    })
-  })
+  //   it('监听函数的this指向自己', function (done) {
+  //     const view = new View
+  //     view.on('test', function () {
+  //       assert.equal(this, view)
+  //       done()
+  //     })
+  //     view.trigger('test')
+  //   })
+  // })
 
   describe('ES5', function () {
     var Base = require('../lib/base.es5')
@@ -51,6 +52,7 @@ describe('实现一个基类，可以继承，可以监听事件', function () {
         }
       })
       var myclass = new MyClass
+
       assert.equal(myclass.getVal(), 'hello world')
       assert.equal(MyClass.say('haha'), 'haha')
       assert.equal(myclass instanceof MyClass, true)
